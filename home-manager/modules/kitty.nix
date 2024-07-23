a69fb6fa3691d96de567965a34332a10ec927827
Andres Hung <andres.hung@outlook.com>
2024-07-23 11:17:07 -0600

    Change font settings

--- home-manager/modules/kitty.nix
+++ home-manager/modules/kitty.nix
@@ -1,8 +1,13 @@
 {
   programs.kitty = {
     enable = true;
+
     settings = {
       confirm_os_window_close = 0;
+    };
+
+    font = {
+      name = "JetBrainsMono Nerd Font";
     };
   };
 }
--- nixos/packages.nix
+++ nixos/packages.nix
@@ -85,6 +85,6 @@
     font-awesome
     powerline-fonts
     powerline-symbols
-    (nerdfonts.override { fonts = [ "NerdFontsSymbolsOnly" ]; })
+    (nerdfonts.override { fonts = [ "JetBrainsMono" ]; })
   ];
 }
