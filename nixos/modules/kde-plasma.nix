428ed74e2bf5fb69b6baff3f68af8a57bebc6706
Andres Hung <andres.hung@outlook.com>
2024-07-24 10:46:00 -0600

    Remove spectacle default KDE program

--- nixos/modules/kde-plasma.nix
+++ nixos/modules/kde-plasma.nix
@@ -1,6 +1,10 @@
-{
+{pkgs, ...}: {
   services.displayManager.sddm.enable = true;
   services.displayManager.sddm.wayland.enable = true;
   services.desktopManager.plasma6.enable = true;
   services.displayManager.defaultSession = "plasma";
+
+  environment.plasma6.excludePackages = with pkgs.kdePackages; [
+    spectacle
+  ];
 }
